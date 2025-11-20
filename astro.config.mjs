caba8acb76799ee1455783d55f3b3bd17d8f0859
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-11-20 22:21:26 +0530

    Remove Tailwind integration to use PostCSS config instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react()],
   devToolbar: {
     enabled: false
   },
