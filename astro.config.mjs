816a632b6f1219e8de0e564bc71a6be6b62f3e68
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-11-20 18:58:27 +0530

    fix: Correct Astro output to 'server' and update Vercel adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   integrations: [react()],
   devToolbar: {
