6885b882cf18e741593e0ce009099a76c919f3f4
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-11-20 19:06:58 +0530

    feat: Switch to Netlify adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [react()],
   devToolbar: {
     enabled: false
