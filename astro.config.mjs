f01689826893b54012d1a1b3fc051b570bb4f025
Aditya Akolkar <aditya.akolkar.here@gmail.com>
2025-11-20 18:53:55 +0530

    fix: Revert to hybrid mode to fix local dev and Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [react()],
   devToolbar: {
     enabled: false
